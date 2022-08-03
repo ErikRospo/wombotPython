4d02bbf2ca1e37872bd5e6c1f4d1f85e03050d57
ErikRospo <rerik.work@gmail.com>
2022-08-03 13:45:34 -0700

    Did stuff

--- using_mod/js/index.js
+++ using_mod/js/index.js
@@ -2,110 +2,139 @@ const Rest = require("./rest.js");
 const identify = require("./identify.js");
 const download = require("./download.js");
 const mkdirp = require("mkdirp");
+// const fs = require("fs");
 const path = require("path");
 
 let paint_rest = new Rest("paint.api.wombo.ai", 100);
 
-module.exports = async function task(prompt, style, update_fn = () => {}, settings = {}) {
-    let {final = true, inter = false, identify_key, download_dir = "./generated/"} = settings;
-    if (final || inter) mkdirp(download_dir);
+module.exports = async function task(
+  prompt,
+  style,
+  update_fn = () => {},
+  settings = {}
+) {
+  let {
+    final = true,
+    inter = false,
+    identify_key,
+    download_dir = "./generated/"
+  } = settings;
+  if (final || inter) {
+    mkdirp(download_dir);
+  }
 
-    let id;
-    try {
-        id = await identify(identify_key);
-    } catch (err) {
-        console.error(err);
-        throw new Error(`Error while sending prompt:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
-    }
+  let id;
+  try {
+    id = await identify(identify_key);
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Error while sending prompt:\n${err.toFriendly
+        ? err.toFriendly()
+        : err.toString()}`
+    );
+  }
 
-    paint_rest.custom_headers = {
-        "Authorization": "bearer " + id,
-        "Origin": "https://app.wombo.art",
-        "Referer": "https://app.wombo.art/",
-    };
+  paint_rest.custom_headers = {
+    Authorization: "bearer " + id,
+    Origin: "https://app.wombo.art",
+    Referer: "https://app.wombo.art/"
+  };
 
-    update_fn({
-        state: "authenticated",
-        id,
-    });
+  update_fn({
+    state: "authenticated",
+    id
+  });
 
-    let task;
-    try {
-        task = await paint_rest.options("/api/tasks/", "POST")
-            .then(() => paint_rest.post("/api/tasks/", {premium: false}));
-    } catch (err) {
-        console.error(err);
-        throw new Error(`Error while allocating a new task:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
-    }
+  let task;
+  try {
+    task = await paint_rest
+      .options("/api/tasks/", "POST")
+      .then(() => paint_rest.post("/api/tasks/", { premium: true }));
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Error while allocating a new task:\n${err.toFriendly
+        ? err.toFriendly()
+        : err.toString()}`
+    );
+  }
 
-    let task_path = "/api/tasks/" + task.id;
+  let task_path = "/api/tasks/" + task.id;
 
-    update_fn({
-        state: "allocated",
-        id,
-        task,
-    });
-    //TODO: Rest of stuff
-    try {
-        task = await paint_rest.options(task_path, "PUT")
-            .then(() => paint_rest.put(task_path, {
-                input_spec: {
-                    display_freq: 10,
-                    prompt,
-                    style: +style,
-                }
-            }));
-    } catch (err) {
-        console.error(err);
-        throw new Error(`Error while sending prompt:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
-    }
-
-    update_fn({
-        state: "submitted",
-        id,
-        task,
-    });
-
-    let inter_downloads = [];
-    let inter_paths = [];
-    let inter_finished = [];
-
-    while (!task.result) {
-        try {
-            task = await paint_rest.get(task_path, "GET");
-        } catch (err) {
-            console.error(err);
-            throw new Error(`Error while fetching update:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
+  update_fn({
+    state: "allocated",
+    id,
+    task
+  });
+  //TODO: Rest of stuff
+  try {
+    task = await paint_rest.options(task_path, "PUT").then(() =>
+      paint_rest.put(task_path, {
+        input_spec: {
+          display_freq: 10,
+          prompt,
+          style: +style
         }
-        if (task.state === "pending") console.warn("Warning: task is pending");
+      })
+    );
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Error while sending prompt:\n${err.toFriendly
+        ? err.toFriendly()
+        : err.toString()}`
+    );
+  }
 
-        if (inter) {
-            for (let n = 0; n < task.photo_url_list.length; n++) {
-                if (inter_downloads[n] || /\/final\.je?pg/i.exec(task.photo_url_list[n])) continue;
-                inter_paths[n] = path.join(download_dir, `${task.id}-${n}.jpg`);
+  update_fn({
+    state: "submitted",
+    id,
+    task
+  });
 
-                inter_downloads[n] = download(task.photo_url_list[n], inter_paths[n]).then(() => {
-                    return inter_finished[n] = inter_paths[n];
-                });
-            }
-        }
+  let inter_downloads = [];
+  let inter_paths = [];
+  let inter_finished = [];
 
-        update_fn({
-            state: "progress",
-            id,
-            task,
-            inter: inter_finished
+  while (!task.result) {
+    try {
+      task = await paint_rest.get(task_path, "GET");
+    } catch (err) {
+      console.error(err);
+      throw new Error(
+        `Error while fetching update:\n${err.toFriendly
+          ? err.toFriendly()
+          : err.toString()}`
+      );
+    }
+    if (task.state === "pending") console.warn("Warning: task is pending");
+
+    if (inter) {
+      for (let n = 0; n < task.photo_url_list.length; n++) {
+        if (
+          inter_downloads[n] ||
+          /\/final\.je?pg/i.exec(task.photo_url_list[n])
+        )
+          continue;
+        inter_paths[n] = path.join(download_dir, `${task.id}-${n}.jpg`);
+
+        inter_downloads[n] = download(
+          task.photo_url_list[n],
+          inter_paths[n]
+        ).then(() => {
+          return (inter_finished[n] = inter_paths[n]);
         });
-        await (new Promise((res) => setTimeout(res, 1000)));
+      }
     }
 
     update_fn({
-        state: "generated",
-        id,
-        task,
-        url: task.result.final,
-        inter: inter_finished,
+      state: "progress",
+      id,
+      task,
+      inter: inter_finished
     });
+<<<<<<< Updated upstream
 
     let download_path = path.join(download_dir, `${task.id}-final.jpg`);
 
@@ -135,11 +164,57 @@ module.exports = async function task(prompt, style, update_fn = () => {}, settin
         inter: inter_finished,
     };
 }
+=======
+    await new Promise(res => setTimeout(res, 1000));
+  }
+
+  update_fn({
+    state: "generated",
+    id,
+    task,
+    url: task.result.final,
+    inter: inter_finished
+  });
+
+  let download_path = path.join(download_dir, `${task.id}-final.jpg`);
+
+  try {
+    console.log(task);
+    if (final) await download(task.result.final, download_path);
+    if (inter) await Promise.all(inter_downloads);
+  } catch (err) {
+    console.error(err);
+    throw new Error(
+      `Error while downloading results:\n${err.toFriendly
+        ? err.toFriendly()
+        : err.toString()}`
+    );
+  }
+
+  update_fn({
+    state: "downloaded",
+    id,
+    task,
+    url: task.result.final,
+    path: final ? download_path : null,
+    inter: inter_finished
+  });
+
+  return {
+    state: "downloaded",
+    id,
+    task,
+    url: task.result.final,
+    path: final ? download_path : null,
+    inter: inter_finished
+  };
+};
+>>>>>>> Stashed changes
 
 module.exports.styles = require("./styles.js");
 module.exports.download = require("./download.js");
 
 // Make `node .` a shorthand for `node cli.js`
 if (require.main === module) {
-    require("./cli.js");
+  require("./cli.js");
 }
--- using_mod/js/styles.js
+++ using_mod/js/styles.js
@@ -1,4 +1,5 @@
 let styles = new Map();
+<<<<<<< Updated upstream
 styles.set(1, "Synthwave");
 styles.set(2, "Ukiyoe");
 styles.set(3, "No Style");
@@ -30,3 +31,62 @@ styles.set(27, "Radioactive");
 styles.set(28, "Arcane");
 
 module.exports = styles;
+=======
+styles.set(34, 'Arcane');
+styles.set(13, 'Baroque');
+styles.set(20, 'Blacklight');
+styles.set(10, 'Dark Fantasy');
+styles.set(14, 'Etching');
+styles.set(5, 'Fantasy Art');
+styles.set(22, 'Ghibli');
+styles.set(7, 'HD');
+styles.set(40, 'Malevolent');
+styles.set(28, 'Melancholic');
+styles.set(11, 'Mystical');
+styles.set(3, 'No Style');
+styles.set(17, 'Provenance');
+styles.set(21, 'Psychedelic');
+styles.set(9, 'Psychic');
+styles.set(27, 'Radioactive');
+styles.set(32, 'Realistic');
+styles.set(18, 'Rose Gold');
+styles.set(15, 'S.Dali');
+styles.set(4, 'Steampunk');
+styles.set(1, 'Synthwave');
+styles.set(35, 'Throwback');
+styles.set(2, 'Ukiyoe');
+styles.set(6, 'Vibrant');
+styles.set(16, 'Wuhtercuhler');
+let steps = new Map();
+
+steps.set(1, 23);
+steps.set(2, 21);
+steps.set(3, 23);
+steps.set(4, 23);
+steps.set(5, 19);
+steps.set(6, 20);
+steps.set(7, 21);
+steps.set(8, 21);
+steps.set(9, 21);
+steps.set(10, 20);
+steps.set(11, 20);
+steps.set(12, 32);
+steps.set(13, 20);
+steps.set(14, 20);
+steps.set(15, 20);
+steps.set(16, 20);
+steps.set(17, 19);
+steps.set(18, 19);
+steps.set(19, 20);
+steps.set(20, 20);
+steps.set(21, 20);
+steps.set(22, 20);
+steps.set(23, 20);
+steps.set(24, 19);
+steps.set(25, 19);
+steps.set(26, 19);
+steps.set(27, 19);
+steps.set(28, 19);
+module.exports = styles;
+module.exports.steps = steps;
+>>>>>>> Stashed changes
