9a87d67a592891ce41f5c291321bb81317786113
ErikRospo <rerik.work@gmail.com>
2022-08-05 17:26:37 -0700

    changed compile_images.py to use settings.json

--- using_mod/sequential/compile_images.py
+++ using_mod/sequential/compile_images.py
@@ -4,15 +4,15 @@ import json
 
 file_path="./settings.json"
 with open(file_path,"rt") as f:
-    settings = json.loads(f)
+    settings = json.loads(f.read())
 
 iterations=settings['iterations']
-style_iterations=settings['style_iterations']
+style_iterations=settings['styleSteps']
 fp='./generated/'+settings['file_folder']
 image_list=[]
 for i in tqdm.trange(iterations):
-    path=fp+str(i)+"/final.jpg"
+    path=fp+"/"+str(i)+"/final.jpg"
     image_list.append(Image.open(path))
 
 final_image=image_list[0]
-final_image.save(fp+"final.gif","GIF",save_all=True,append_images=image_list[1:],duration=100,loop=0,optimize=True)
+final_image.save(fp+"/final.gif","GIF",save_all=True,append_images=image_list[1:],duration=100,loop=0,optimize=True)
