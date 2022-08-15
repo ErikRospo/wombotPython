2172da69bef3973796a9577e45efd7b6b2ad6193
Erik Rospo <rerik.work@gmail.com>
2022-08-15 12:51:27 -0700

    More Changes to how the video is made

--- using_mod/videoMaker/Second Way/make_video.py
+++ using_mod/videoMaker/Second Way/make_video.py
@@ -14,7 +14,7 @@ for filename in paths:
     imgArray.append(cv2.imread(filename))
 print("done reading")
 print("writing video")
-out = cv2.VideoWriter(osp.join(osp.dirname(path),"final.mp4"),cv2.VideoWriter_fourcc(*'mp4v'), 15, [960,1568])
+out = cv2.VideoWriter(osp.join(osp.dirname(path),"final.mp4"),cv2.VideoWriter_fourcc(*'mp4v'), 8, [960,1568])
 for i in range(len(imgArray)):
     out.write(imgArray[i])
 out.release()
