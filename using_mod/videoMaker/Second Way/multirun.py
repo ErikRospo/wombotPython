d0744d3b2beb0d42c39fccf5b91c96b6eb46d555
ErikRospo <rerik.work@gmail.com>
2022-08-18 21:04:58 -0700

    made some changes to multirun

--- using_mod/videoMaker/Second Way/multirun.py
+++ using_mod/videoMaker/Second Way/multirun.py
@@ -4,13 +4,14 @@ import threading
 cmd="python3 run.py"
 c=0
 wasError=False
-tnum=5
+tnum=8
 def run_prog():
     res=os.system(cmd)
     if res!=0:
         global wasError
         wasError=True
         print("Error:"+str(res))
+s=time.time_ns()
 while not wasError:
     ts=[]
     for i in range(0,tnum):
@@ -22,6 +23,13 @@ while not wasError:
     for i in range(0,tnum):
         ts[i].join()
     c+=tnum
+e=time.time_ns()
+dur_ns=e-s
+dur=dur_ns/10**9
 
 print("DONE")
 print("Ran "+str(c)+" times")
+exhours=int((dur)/3600)
+exminutes=int(((dur)%3600)/60)
+exseconds=int(((dur)%3600)%60)
+print(f"Over: {exhours}h {exminutes}m {exseconds}s")
