4b092e339b747c5c35d0d5139be97f3592593ba4
Erik Rospo <rerik.work@gmail.com>
2022-10-14 13:53:30 -0700

    More updates

--- using_mod/videoMaker/Second Way/identify.js
+++ using_mod/videoMaker/Second Way/identify.js
@@ -1,42 +1,55 @@
-const Rest = require('./rest.js')
-const fs = require('fs')
-const path = require('path')
-const SECRET_PATH = path.join(path.dirname(__filename), 'secret.json')
+const Rest = require("./rest.js");
+const fs = require("fs");
+const path = require("path");
 
-let identifyHostname = 'identitytoolkit.googleapis.com'
-let identifySecretKey = 'AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw'
+const SECRET_PATH = path.join(path.dirname(__filename), "secret.json");
+
+let identify_hostname = "identitytoolkit.googleapis.com";
+let identify_secret_key = "AIzaSyDCvp5MTJLUdtBYEKYWXJrlLzu1zuKM6Xw";
 
 if (fs.existsSync(SECRET_PATH)) {
-  let secret = JSON.parse(fs.readFileSync(SECRET_PATH, 'utf8'))
+    let secret = JSON.parse(fs.readFileSync(SECRET_PATH, "utf8"));
+	
 
-  if (secret.identify_key) identifySecretKey = secret.identify_key
-  if (secret.identify_hostname) identifyHostname = secret.identify_hostname
+    if (secret.identify_key) identify_secret_key = secret.identify_key;
+    if (secret.identify_hostname) identify_hostname = secret.identify_hostname;
 }
 
-let identifyRest = new Rest(identifyHostname)
+let identify_rest = new Rest(identify_hostname);
 
-let identifyCache
-function identify (identifyKey) {
-  if (!identifyKey) {
-    if (identifySecretKey) {
-      identifyKey = identifySecretKey
-    } else {
-      throw new Error('No identify key provided and no secret.json found!')
+let identify_cache;
+let identify_timeout = 0;
+function identify(identify_key) {
+    if (!identify_key) {
+        if (identify_secret_key) {
+            identify_key = identify_secret_key;
+        } else {
+            throw new Error("No identify key provided and no secret.json found!");
+        }
     }
-  }
-
-  // eslint-disable-next-line no-unused-vars, no-async-promise-executor
-  return new Promise(async (resolve, _reject) => {
-    let res = await identifyRest.post(
-      '/v1/accounts:signUp?key=' + identifyKey,
-      {
-        key: identifyKey
-      }
-    )
-    // console.log(res)
-    identifyCache = res.idToken
-    resolve(identifyCache)
-  })
+	let d;
+	if (fs.existsSync("./identityToolkit.json")){
+	d=JSON.parse(fs.readFileSync("./identityToolkit.json"))
+	}
+	else{
+		d={timeout:0}
+	}
+    if (d.timeout<new Date().getTime()){
+        return new Promise(async (resolve, reject) => {
+            let res = await identify_rest.post("/v1/accounts:signUp?key=" + identify_key, {
+                key: identify_key
+            });
+            identify_cache = res.idToken;
+            identify_timeout = new Date().getTime() + 1000 * +res.expiresIn;
+            fs.writeFileSync("./identityToolkit.json",JSON.stringify({"id":res.idToken,"timeout":identify_timeout}))
+		resolve(identify_cache);
+        });
+    } else {
+	identify_cache=d.id;
+        return new Promise((resolve) => {
+            resolve(identify_cache);
+        });
+    }
 }
 
-module.exports = identify
+module.exports = identify;
--- using_mod/videoMaker/Second Way/index.js
+++ using_mod/videoMaker/Second Way/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-constant-condition */
-const Rest = require('./rest.js')
-const identify = require('./identify.js')
-const download = require('./download.js')
-const mkdirp = require('mkdirp')
-const path = require('path');
-const { writeFile, writeFileSync } = require('fs');
-
-let paintRest = new Rest('paint.api.wombo.ai', 100)
-let imagePaintRest = new Rest('www.wombo.art', 100)
+const Rest = require("./rest.js");
+const identify = require("./identify.js");
+const download = require("./download.js");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const { writeFile, writeFileSync } = require("fs");
+const updateStyles=require("./get_next_data.js");
+let paintRest = new Rest('paint.api.wombo.ai', 100);
+let imagePaintRest = new Rest('www.wombo.art', 100);
 
 /**
  * @param {string} prompt
@@ -35,7 +35,8 @@ module.exports.task = async function runTask (
     imageWeight = 'HIGH'
   } = inputImageArg
   let id
-  let prefix = _prefix
+  let prefix = _prefix;
+  if (final || inter) mkdirp(downloadDir);
   try {
     id = await identify()
   } catch (err) {
@@ -74,21 +75,10 @@ module.exports.task = async function runTask (
     mediastoreid = res.mediastore_uid
   }
   paintRest.customHeaders = {
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9,en-GB-oxendict;q=0.8,en-AU;q=0.7",
-    "authorization": `bearer ${id}`,
-    "cache-control": "no-cache",
-    "pragma": "no-cache",
-    "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "x-app-version": "WEB-1.90.1",
+    "Authorization": `bearer ${id}`,
+    "Origin": "https://app.wombo.art",
+
     "Referer": "https://app.wombo.art/",
-    "Referrer-Policy": "strict-origin-when-cross-origin",
-    "origin":"https://app.wombo.art/"
   },
 
   updateFn({
@@ -97,35 +87,15 @@ module.exports.task = async function runTask (
   })
 
   let task
-  let taskPath
   try {
-//     fetch("https://paint.api.wombo.ai/api/tasks/86c5f074-f733-45a3-80af-bfa0d1ad28d2", {
-//   "headers": {
-//     "accept": "*/*",
-//     "accept-language": "en-US,en;q=0.9,en-GB-oxendict;q=0.8,en-AU;q=0.7",
-//     "authorization": "bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImVkNmJjOWRhMWFmMjM2ZjhlYTU2YTVkNjIyMzQwMWZmNGUwODdmMTEiLCJ0eXAiOiJKV1QifQ.eyJwcm92aWRlcl9pZCI6ImFub255bW91cyIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9wYWludC1wcm9kIiwiYXVkIjoicGFpbnQtcHJvZCIsImF1dGhfdGltZSI6MTY2MjE4MjUxOSwidXNlcl9pZCI6ImtnRnd4MURwTW1UN1c4OWswN1p0TzFoYTZmbjIiLCJzdWIiOiJrZ0Z3eDFEcE1tVDdXODlrMDdadE8xaGE2Zm4yIiwiaWF0IjoxNjYyNDExMDQ5LCJleHAiOjE2NjI0MTQ2NDksImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnt9LCJzaWduX2luX3Byb3ZpZGVyIjoiYW5vbnltb3VzIn19.gHPKGmyRPvBuZM5RBFPbxGPQDH3XixcfRVCsaHaKGx94dt85rrf6_sDQj5FyW0LM8XTct7Ao2zIeKzlZf3SnIMVLOZQBTf8_GXt42eqoDy4r8b1wInLaaakyPiPQOlfwojBiAF2Id2_70aa9iZZZIeUKhKaF57KHl_o1D3yeJdHFJvjs9gbA_eqS57XJt13Md7MhzaZW00FijXPat-rwHgYQTngb-CU1md8RMJRxpyCFa-881mJ0jpxAwrfQEozo1GmOURYTzKPRHxAgyB7_Xr-KxsdnsnsVvivZQpy9TY-2S7K4wNOwDGOdQzzrJtnffBSzJiFf2G5bhdV7-dS2Dw",
-//     "cache-control": "no-cache",
-//     "pragma": "no-cache",
-//     "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
-//     "sec-ch-ua-mobile": "?0",
-//     "sec-ch-ua-platform": "\"Windows\"",
-//     "sec-fetch-dest": "empty",
-//     "sec-fetch-mode": "cors",
-//     "sec-fetch-site": "cross-site",
-//     "x-app-version": "WEB-1.90.1",
-//     "Referer": "https://app.wombo.art/",
-//     "Referrer-Policy": "strict-origin-when-cross-origin"
-//   },
-//   "body": null,
-//   "method": "GET"
-// });
-    task = await paintRest.post('/api/tasks/', { premium: false },"POST",{"content-type":"text/plain;charset=utf-8"})
-    taskPath = '/api/tasks/' + task.id
+    task = await paintRest.options("/api/tasks/", "POST")
+    .then(() => paintRest.post("/api/tasks/", {premium: false}));
   } catch (err) {
-    console.log(err)
-    return await runTask(prompt, style, updateFn, settings, inputImage)
+    console.log(err);
+    throw new Error(`Error while allocating a new task:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);    
   }
 
+  let taskPath="/api/tasks/"+task.id
   updateFn({
     state: 'allocated',
     id,
@@ -140,49 +110,45 @@ module.exports.task = async function runTask (
       prompt,
       style: +style
     }
-  }
-
+  };
   if (inputImage) {
-    // eslint-disable-next-line camelcase
-    inputObject.input_spec.input_image = {
       // eslint-disable-next-line camelcase
-      weight: imageWeight,
-      // eslint-disable-next-line camelcase
-      mediastore_id: mediastoreid
-    }
-  }
-  let ti = 1000
-  //console.log(`https://${paintRest.hostname}${taskPath}`)
-  while (!task) {
+      inputObject.input_spec.input_image = {
+        // eslint-disable-next-line camelcase
+        weight: imageWeight,
+        // eslint-disable-next-line camelcase
+        mediastore_id: mediastoreid
+      };
+    } 
+
+
     try {
       task = await paintRest
         .options(taskPath, 'PUT')
-        .then(() => paintRest.put(taskPath, inputObject,{"content-type":"text/plain;charset=utf-8"}))
-      updateFn({
-        state: 'submitted',
-        id,
-        task
-      })
+        .then(() => paintRest.put(taskPath, inputObject))
     } catch (error) {
-      updateFn({
-        state: 'error',
-        id,
-        task,
-        message: error.toFriendly(),
-        times: ti
-      })
-      ti *= 2
-      await new Promise(res => setTimeout(res, ti))
+          console.error(err);
+          throw new Error(`Error while sending prompt:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
     }
-  }
-  // eslint-disable-next-line no-undefined
+    updateFn({
+      state: "submitted",
+      id,
+      task
+    })
+
+    // eslint-disable-next-line no-undefined
   let interDownloads = []
   let interPaths = []
   let interFinished = []
-  while (task.result===null) {
+  while (!task.result) {
     try {
       task = await paintRest.get(taskPath, 'GET')
         
+    }catch(err){
+
+      console.log(err);
+      throw new Error(`Error while fetching update:\n${err.toFriendly ? err.toFriendly() : err.toString()}`);
+      
     }
     if (inter) {
       await mkdirp(`${downloadDir}/`)
@@ -206,7 +172,7 @@ module.exports.task = async function runTask (
       task,
       inter: interFinished
     })
-    await new Promise(res => setTimeout(res, 1000))
+    await new Promise(res => setTimeout(res, 1500))
   }
   updateFn({
     state: 'generated',
