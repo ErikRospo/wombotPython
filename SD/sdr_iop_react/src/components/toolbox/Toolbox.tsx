afbf848d583e8e727fed17d1017799134023dc4f
ErikRospo <rerik.work@gmail.com>
2022-12-18 22:46:21 -0800

    Fixed visibility issues in the toolbox menu

--- SD/sdr_iop_react/src/components/toolbox/Toolbox.tsx
+++ SD/sdr_iop_react/src/components/toolbox/Toolbox.tsx
@@ -29,35 +29,38 @@ export class Toolbox extends React.Component {
                     </div>
                     <br />
                     <div id="selector" style={{ "opacity": (this.state.closed ? "0%" : "100%") }}>
-                        <label>
-                            <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 0 })} checked={this.state.tool === 0} />
-                            Eraser <BsEraserFill></BsEraserFill>
-                        </label>
-                        <br />
-                        <label>
-                            <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 1 })} checked={this.state.tool === 1} />
 
-                            Pen <BsPenFill></BsPenFill>
-                        </label><br />
-                        <label>
-                            <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 2 })} checked={this.state.tool === 2} />
+                        <div >
+                            <label>
+                                <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 0 })} checked={this.state.tool === 0} />
+                                Eraser <BsEraserFill></BsEraserFill>
+                            </label>
+                            <br />
+                            <label>
+                                <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 1 })} checked={this.state.tool === 1} />
 
-                            Fill <TfiPaintBucket></TfiPaintBucket>
-                        </label>
-                        <div></div>
+                                Pen <BsPenFill></BsPenFill>
+                            </label><br />
+                            <label>
+                                <input type="radio" name="Tool" className="toolSelectorButton" onClick={() => this.setState({ tool: 2 })} checked={this.state.tool === 2} />
+
+                                Fill <TfiPaintBucket></TfiPaintBucket>
+                            </label>
+                            <div></div>
+                        </div>
+                        <hr />
+                        <section>
+                            <div id="Radius" >
+                                <label htmlFor="RadiusInput">Radius: </label>
+                                <input type="number" name="Radius" id="RadiusInput" onChange={(ev) => { this.setState({ radius: ev.target.valueAsNumber }); }} defaultValue={this.state.radius} />
+                            </div>
+                            <br />
+                            <div id="Color" >
+                                <label htmlFor="ColorInput">Color: </label>
+                                <input type="color" name="Color" id="ColorInput" onChange={(ev) => { this.setState({ color: ev.target.value }) }} />
+                            </div>
+                        </section>
                     </div>
-                    <hr />
-                    <section>
-                        <div id="Radius" >
-                            <label htmlFor="RadiusInput">Radius: </label>
-                            <input type="number" name="Radius" id="RadiusInput" onChange={(ev) => { this.setState({ radius: ev.target.valueAsNumber }); }} defaultValue={this.state.radius} />
-                        </div>
-                        <br />
-                        <div id="Color" >
-                            <label htmlFor="ColorInput">Color: </label>
-                            <input type="color" name="Color" id="ColorInput" onChange={(ev) => { this.setState({ color: ev.target.value }) }} />
-                        </div>
-                    </section>
                 </div>
             </div>
         )
